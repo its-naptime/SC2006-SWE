2c13bb9304e700503edcbc7deef828e66dafd07a
jordi2987 <90922512+jordi2987@users.noreply.github.com>
2024-10-13 20:57:40 +0800

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,31 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissons import IsAuthenticated, AllowAny
+from .models import Note
 
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+        
+class NoteDelete(generics.destroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+        
 # Create your own views here
 class createUserView(generics.CreateAPIView):
     queryset = User.objects.all()
