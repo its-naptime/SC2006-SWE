fbac2dc0fa0ba78907b6280f021cd7e0f9f52b47
jordi2987 <90922512+jordi2987@users.noreply.github.com>
2024-11-07 21:29:20 +0800

    Update Form.js

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import api from "../Api";
 import LoadingIndicator from "./LoadingIndicator"; // Ensure this path is correct
 
@@ -8,11 +9,11 @@ function Form({ activeTab, setActiveTab }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [registerDetails, setRegisterDetails] = useState({
-    name: "",
+    //name: "",
     username: "",
     email: "",
     password: "",
-    confirmPassword: "",
+    //confirmPassword: "",
   });
   const router = useRouter();
 
@@ -24,9 +25,9 @@ function Form({ activeTab, setActiveTab }) {
     setLoading(true);
     e.preventDefault();
     try {
-      const res = await api.post("/api/token/", { username, password });
-      localStorage.setItem("ACCESS_TOKEN", res.data.access);
-      localStorage.setItem("REFRESH_TOKEN", res.data.refresh);
+      const res = await api.post("/api/token/", { username: username, password: password });
+      localStorage.setItem(ACCESS_TOKEN, res.data.access);
+      localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
       router.push("/");
     } catch (error) {
       alert("Login failed: " + error.message);
@@ -38,8 +39,20 @@ function Form({ activeTab, setActiveTab }) {
   const handleRegisterSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
+
+    // Check if passwords match
+    if (registerDetails.password != registerDetails.confirmPassword) {
+      alert("Passwords do not match");
+      setLoading(false);
+      return;
+    }
+
     try {
-      await api.post("/api/user/register/", registerDetails);
+      await api.post("/api/user/register/", {
+        username: registerDetails.username,
+        email: registerDetails.email,
+        password: registerDetails.password,
+      });
       alert("Registration successful");
       setActiveTab("login");
     } catch (error) {
