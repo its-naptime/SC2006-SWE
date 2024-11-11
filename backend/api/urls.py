8d2cd33c996ab9f29c7013c6ed7d555cf2c5710f
jordi2987 <90922512+jordi2987@users.noreply.github.com>
2024-11-11 20:41:05 +0800

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import RequestPasswordResetView
 
 urlpatterns = [
+        path('api/user/reset-password/', RequestPasswordResetView.as_view(), name='password-reset-request'),
 
 ]
