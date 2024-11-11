559cb95a1a25c7feb4157d5f937e93b97e1d532f
Elijah <elim084@e.ntu.edu.sg>
2024-11-11 15:04:09 +0800

    make templates

--- backend/catalogue/serializers.py
+++ backend/catalogue/serializers.py
@@ -4,14 +4,48 @@ from database.models import UserSchoolSearch, UserPreschoolSearch, UserHDBSearch
 class UserSchoolSearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserSchoolSearch
-        fields = '__all__'
+        fields = [
+            'search_query', 'school_name', 'url_address', 'address', 'postal_code',
+            'telephone_no', 'telephone_no_2', 'fax_no', 'fax_no_2', 'email_address',
+            'mrt_desc', 'bus_desc', 'principal_name', 'first_vp_name', 'second_vp_name',
+            'third_vp_name', 'fourth_vp_name', 'fifth_vp_name', 'sixth_vp_name',
+            'dgp_code', 'zone_code', 'type_code', 'nature_code', 'session_code',
+            'mainlevel_code', 'sap_ind', 'autonomous_ind', 'gifted_ind', 'ip_ind',
+            'mothertongue1_code', 'mothertongue2_code', 'mothertongue3_code'
+        ]
+
 
 class UserPreschoolSearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserPreschoolSearch
-        fields = '__all__'
+        fields = [
+            'search_query', 'tp_code', 'centre_code', 'centre_name', 'organisation_code',
+            'organisation_description', 'service_model', 'centre_contact_no', 'centre_email_address',
+            'centre_address', 'postal_code', 'centre_website', 'infant_vacancy_current_month',
+            'infant_vacancy_next_month', 'infant_vacancy_third_month', 'infant_vacancy_fourth_month',
+            'infant_vacancy_fifth_month', 'infant_vacancy_sixth_month', 'infant_vacancy_seventh_month',
+            'pg_vacancy_current_month', 'pg_vacancy_next_month', 'pg_vacancy_third_month',
+            'pg_vacancy_fourth_month', 'pg_vacancy_fifth_month', 'pg_vacancy_sixth_month',
+            'pg_vacancy_seventh_month', 'n1_vacancy_current_month', 'n1_vacancy_next_month',
+            'n1_vacancy_third_month', 'n1_vacancy_fourth_month', 'n1_vacancy_fifth_month',
+            'n1_vacancy_sixth_month', 'n1_vacancy_seventh_month', 'n2_vacancy_current_month',
+            'n2_vacancy_next_month', 'n2_vacancy_third_month', 'n2_vacancy_fourth_month',
+            'n2_vacancy_fifth_month', 'n2_vacancy_sixth_month', 'n2_vacancy_seventh_month',
+            'k1_vacancy_current_month', 'k1_vacancy_next_month', 'k1_vacancy_third_month',
+            'k1_vacancy_fourth_month', 'k1_vacancy_fifth_month', 'k1_vacancy_sixth_month',
+            'k1_vacancy_seventh_month', 'k2_vacancy_current_month', 'k2_vacancy_next_month',
+            'k2_vacancy_third_month', 'k2_vacancy_fourth_month', 'k2_vacancy_fifth_month',
+            'k2_vacancy_sixth_month', 'k2_vacancy_seventh_month', 'food_offered',
+            'second_languages_offered', 'spark_certified', 'weekday_full_day', 'saturday',
+            'scheme_type', 'extended_operating_hours', 'provision_of_transport', 'government_subsidy',
+            'gst_registration', 'last_updated', 'remarks'
+        ]
 
 class UserHDBSearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserHDBSearch
-        fields = '__all__'
+        fields = [
+            'search_query', 'month', 'town', 'flat_type', 'block', 'street_name',
+            'storey_range', 'floor_area_sqm', 'flat_model', 'lease_commence_date',
+            'remaining_lease', 'resale_price'
+        ]
--- backend/catalogue/views.py
+++ backend/catalogue/views.py
@@ -10,19 +10,35 @@ class UserSchoolSearchView(viewsets.ModelViewSet):
     queryset = UserSchoolSearch.objects.all()
     serializer_class = UserSchoolSearchSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    def get_queryset(self):
+        return UserSchoolSearch.objects.filter(user=self.request.user)
+
+
 class UserPreschoolSearchView(viewsets.ModelViewSet):
     queryset = UserPreschoolSearch.objects.all()
     serializer_class = UserPreschoolSearchSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    def get_queryset(self):
+        return UserPreschoolSearch.objects.filter(user=self.request.user)
+
 class UserHDBSearchView(viewsets.ModelViewSet):
     queryset = UserHDBSearch.objects.all()
     serializer_class = UserHDBSearchSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    def get_queryset(self):
+        return UserHDBSearch.objects.filter(user=self.request.user)
     
 # List View
 @login_required
 def user_school_search_list(request):
     searches = UserSchoolSearch.objects.filter(user=request.user)
-    return render(request, 'catalogue/user_school_search_list.html', {'searches': searches})
+    return render(request, 'catalogue/templates/user_school_search_list.html', {'searches': searches})
 
 # Create View
 @login_required
@@ -36,7 +52,7 @@ def user_school_search_create(request):
             return redirect('user_school_search_list')
     else:
         form = UserSchoolSearchForm()
-    return render(request, 'catalogue/user_school_search_form.html', {'form': form})
+    return render(request, 'catalogue/templates/user_school_search_form.html', {'form': form})
 
 # Delete View
 @login_required
@@ -45,18 +61,18 @@ def user_school_search_delete(request, pk):
     if request.method == 'POST':
         search.delete()
         return redirect('user_school_search_list')
-    return render(request, 'catalogue/user_school_search_confirm_delete.html', {'search': search})
+    return render(request, 'catalogue/templates/user_school_search_confirm_delete.html', {'search': search})
 
 # Detail View
 @login_required
 def user_school_search_detail(request, pk):
     search = get_object_or_404(UserSchoolSearch, pk=pk, user=request.user)
-    return render(request, 'catalogue/user_school_search_detail.html', {'search': search})
+    return render(request, 'catalogue/templates/user_school_search_detail.html', {'search': search})
 
 @login_required
 def user_preschool_search_list(request):
     searches = UserPreschoolSearch.objects.filter(user=request.user)
-    return render(request, 'catalogue/user_preschool_search_list.html', {'searches': searches})
+    return render(request, 'catalogue/templates/user_preschool_search_list.html', {'searches': searches})
 
 # Create View
 @login_required
@@ -70,13 +86,13 @@ def user_preschool_search_create(request):
             return redirect('user_preschool_search_list')
     else:
         form = UserPreschoolSearchForm()
-    return render(request, 'catalogue/user_preschool_search_form.html', {'form': form})
+    return render(request, 'catalogue/templates/user_preschool_search_form.html', {'form': form})
 
 # Detail View
 @login_required
 def user_preschool_search_detail(request, pk):
     search = get_object_or_404(UserPreschoolSearch, pk=pk, user=request.user)
-    return render(request, 'catalogue/user_preschool_search_detail.html', {'search': search})
+    return render(request, 'catalogue/templates/user_preschool_search_detail.html', {'search': search})
 
 # Delete View
 @login_required
@@ -85,12 +101,12 @@ def user_preschool_search_delete(request, pk):
     if request.method == 'POST':
         search.delete()
         return redirect('user_preschool_search_list')
-    return render(request, 'catalogue/user_preschool_search_confirm_delete.html', {'search': search})
+    return render(request, 'catalogue/templates/user_preschool_search_confirm_delete.html', {'search': search})
 
 @login_required
 def user_hdb_search_list(request):
     searches = UserHDBSearch.objects.filter(user=request.user)
-    return render(request, 'catalogue/user_hdb_search_list.html', {'searches': searches})
+    return render(request, 'catalogue/templates/user_hdb_search_list.html', {'searches': searches})
 
 # Create View
 @login_required
@@ -104,13 +120,13 @@ def user_hdb_search_create(request):
             return redirect('user_hdb_search_list')
     else:
         form = UserHDBSearchForm()
-    return render(request, 'catalogue/user_hdb_search_form.html', {'form': form})
+    return render(request, 'catalogue/templates/user_hdb_search_form.html', {'form': form})
 
 # Detail View
 @login_required
 def user_hdb_search_detail(request, pk):
     search = get_object_or_404(UserHDBSearch, pk=pk, user=request.user)
-    return render(request, 'catalogue/user_hdb_search_detail.html', {'search': search})
+    return render(request, 'catalogue/templates/user_hdb_search_detail.html', {'search': search})
 
 # Delete View
 @login_required
@@ -119,4 +135,4 @@ def user_hdb_search_delete(request, pk):
     if request.method == 'POST':
         search.delete()
         return redirect('user_hdb_search_list')
-    return render(request, 'catalogue/user_hdb_search_confirm_delete.html', {'search': search})
+    return render(request, 'catalogue/templates/user_hdb_search_confirm_delete.html', {'search': search})
