366f4eb7e4bf3c6e5a17e7cdda8ac11400e03af5
jordi2987 <90922512+jordi2987@users.noreply.github.com>
2024-11-11 20:43:56 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -161,3 +161,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Email address from environment variable
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Email password or app password from environment variable
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')  # Default email sender
